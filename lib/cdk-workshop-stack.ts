ff071f757d24095722f40a01cc0b8b05f3f244b1
anmolag44 <anmolagarwalmrt@gmail.com>
2023-02-15 16:43:27 +0530

    define lambda as a resource and map and deploy it

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // nothing here!
+    // define an AWS Lambda as a resource
+    const hello = new lambda.Function(this, 'HelloHandler',{
+      runtime: lambda.Runtime.NODEJS_16_X, // this is the execution environment
+      code: lambda.Code.fromAsset('lambda'), // this is the code to be loaded from "lambda" directory
+      handler: 'hello.handler' // file is "hello" & function is "handler"
+    });
   }
 }
