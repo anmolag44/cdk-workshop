41d63344365690622fc99c9c18e0c99da9e97e40
anmolag44 <anmolagarwalmrt@gmail.com>
2023-02-21 13:20:33 +0530

    added hitcounter construct, lambda and bind it to the apigatewway endpoint and then we use hitcounter-construct in the main stack

--- lib/hitcounter-construct.ts
+++ lib/hitcounter-construct.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
 import {Construct} from 'constructs';
 
 export interface HitCounterProps {
@@ -8,10 +9,24 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
-    constructor(scope: Construct, id: string, props?: HitCounterProps){
+    // allowing access to the counter function **
+    public readonly handler: lambda.Function; 
+
+    constructor(scope: Construct, id: string, props: HitCounterProps){
         super(scope, id);
-        
-        // TODO
- 
+    
+        const table = new cdk.aws_dynamodb.Table(this, 'Hits', {
+            partitionKey: {name: 'path', type: cdk.aws_dynamodb.AttributeType.STRING}  // creating a db resource with path as partition key 
+        });
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,  
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hitcounter.handler',  // giving handler from lambda hitcounter.js
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        });
     }
 }
